40621c88cad09acd3b7be47e0d22243d00d7e596
Christoph Reimers <christoph@superservice-international.com>
2023-10-27 16:14:51 +0200

    Revert "redesign landing page"
    This reverts commit 94508b091b730a327157987b0b54b6ad55c11b67.

--- app/(text-views)/layout.tsx
+++ app/(text-views)/layout.tsx
@@ -1,5 +1,3 @@
-import Footer from "@/components/layout/footer";
-
 export default function TextPageLayout({
   children,
 }: {
@@ -10,7 +8,41 @@ export default function TextPageLayout({
       <main className="max-w-prose mx-auto my-20 md:my-28 px-4 md:px-0 flex-1 w-full">
         {children}
       </main>
-      <Footer />
+      <footer className="bg-green-900 p-8 text-white font-light">
+        <div className="max-w-4xl mx-auto w-full grid grid-cols-1 md:grid-cols-4 gap-8">
+          <div>
+            <h3 className="mb-2 font-semibold text-lg">Ethiopia Office</h3>
+            <p>
+              ILRI Sholla Campus <br />
+              CMC Road <br />
+              Addis Ababa
+            </p>
+          </div>
+          <div>
+            <h3 className="mb-2 font-semibold text-lg">Kenia Office</h3>
+            <p>
+              ICRAF House <br />
+              United Nations Avenue, Gigiri <br />
+              Nairobi
+            </p>
+          </div>
+          <div>
+            <h3 className="mb-2 font-semibold text-lg">Zimbabwe Office</h3>
+            <p>
+              12.5 KM Peg, Mazowe Road, Mount Pleasant <br />
+              Harare
+            </p>
+          </div>
+          <div>
+            <h3 className="mb-2 font-semibold text-lg">Email</h3>
+            <p>
+              <a href="mailto:gaia@cgiar.org" className="hover:underline">
+                gaia@cgiar.org
+              </a>
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
