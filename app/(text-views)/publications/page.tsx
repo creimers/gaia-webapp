ef98e4e2972260501f2dddf4de053fa792834813
Christoph Reimers <christoph@superservice-international.com>
2023-11-07 13:20:18 +0100

    Add publication filter component to Publications page

--- app/(text-views)/publications/page.tsx
+++ app/(text-views)/publications/page.tsx
@@ -2,8 +2,8 @@ import client from "@/tina/__generated__/client";
 import type { Publication } from "@/tina/__generated__/types";
 
 import PublicationItem from "@/components/publication";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
+
+import Filter from "@/components/publications/filter";
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -32,26 +32,7 @@ export default async function Publications({ searchParams }: Props) {
     <div>
       <div className="prose">
         <h1>Publications</h1>
-        <div className="not-prose space-y-2 mb-4">
-          <span className="text-sm">Filter by publication type</span>
-          <div className="flex space-x-4">
-            {PUBLICATION_TYPES.map((type) => {
-              const active = searchParams.type === type;
-              return (
-                <a
-                  key={type}
-                  href={active ? `/publications` : `/publications?type=${type}`}
-                  className={cn(
-                    "rounded-md px-2 py-1",
-                    active ? "bg-lime-600 text-white" : "bg-gray-100 text-black"
-                  )}
-                >
-                  {type}
-                </a>
-              );
-            })}
-          </div>
-        </div>
+        <Filter />
         <div className="space-y-4 not-prose">
           {themPublications.map((pub) => {
             return <PublicationItem key={pub.id} publication={pub} />;
---
+++ components/publications/filter.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const PUBLICATION_TYPES = ["Journal", "Technical Report", "Presentation"];
+
+export default function Filter() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const publicationType = searchParams.get("type");
+
+  function handleClick(type: string) {
+    const params = new URLSearchParams(searchParams);
+    if (publicationType === type) {
+      params.delete("type");
+    } else {
+      params.set("type", type);
+    }
+    router.replace("/publications?" + params.toString());
+  }
+
+  return (
+    <div className="not-prose space-y-2 mb-4">
+      <span className="text-sm">Filter by publication type</span>
+      <div className="flex space-x-4">
+        {PUBLICATION_TYPES.map((type) => {
+          const active = publicationType === type;
+          return (
+            <span
+              onClick={() => handleClick(type)}
+              key={type}
+              className={cn(
+                "rounded-md px-2 py-1 text-xs sm:text-sm md:text-base cursor-pointer",
+                active ? "bg-lime-600 text-white" : "bg-gray-100 text-black"
+              )}
+            >
+              {type}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
