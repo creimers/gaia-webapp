2598770616cb8348034f3bf216bc271247c94202
Christoph Reimers <christoph@superservice-international.com>
2023-11-30 09:28:37 +0100

    move publication filtering to the client

--- app/(text-views)/publications/page.tsx
+++ app/(text-views)/publications/page.tsx
@@ -1,44 +1,23 @@
 import client from "@/tina/__generated__/client";
 import type { Publication } from "@/tina/__generated__/types";
 
-import PublicationItem from "@/components/publication";
-
 import Filter from "@/components/publications/filter";
+import PublicationList from "./publication-list";
 
-type Props = {
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-const PUBLICATION_TYPES = ["Journal", "Technical Report", "Presentation"];
-
-export default async function Publications({ searchParams }: Props) {
+export default async function Publications() {
   const res = await client.queries.publicationConnection({ sort: "year" });
   let publications = (res.data.publicationConnection?.edges || []).map(
     (pub) => pub?.node
   );
   publications = publications.reverse();
   let themPublications = publications as Publication[];
-  // if (
-  //   searchParams.type &&
-  //   PUBLICATION_TYPES.includes(searchParams.type as string)
-  // ) {
-  //   console.log(searchParams.type);
-  //   themPublications = themPublications.filter((pub) => {
-  //     return pub.publicationType[0] === searchParams.type;
-  //   });
-  // }
 
   return (
     <div>
       <div className="prose">
         <h1>Publications</h1>
         <Filter />
-        <div className="space-y-4 not-prose">
-          {themPublications.map((pub) => {
-            return <PublicationItem key={pub.id} publication={pub} />;
-          })}
-        </div>
-        {themPublications.length === 0 && <p>No publications available.</p>}
+        <PublicationList publications={themPublications} />
       </div>
     </div>
   );
---
+++ app/(text-views)/publications/publication-list.tsx
@@ -0,0 +1,31 @@
+"use client";
+import type { Publication } from "@/tina/__generated__/types";
+import PublicationItem from "@/components/publication";
+import { useSearchParams } from "next/navigation";
+
+const PUBLICATION_TYPES = ["Journal", "Technical Report", "Presentation"];
+
+type Props = {
+  publications: Publication[];
+};
+export default function PublicationList({ publications }: Props) {
+  const searchParams = useSearchParams();
+  let filteredPublications = publications as Publication[];
+  const publicationType = searchParams.get("type");
+  if (
+    publicationType &&
+    PUBLICATION_TYPES.includes(publicationType as string)
+  ) {
+    filteredPublications = filteredPublications.filter((pub) => {
+      return pub.publicationType[0] === publicationType;
+    });
+  }
+  return (
+    <div className="space-y-4 not-prose">
+      {filteredPublications.map((pub) => {
+        return <PublicationItem key={pub.id} publication={pub} />;
+      })}
+      {filteredPublications.length === 0 && <p>No publications available.</p>}
+    </div>
+  );
+}
