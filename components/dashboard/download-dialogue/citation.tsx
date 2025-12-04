1f7e90674a84b99a0b5d56e8b1b9c65c1c82216f
Christoph Reimers <christoph@superservice-international.com>
2025-12-04 11:27:17 +0100

    update citation

--- components/dashboard/download-dialogue/citation.tsx
+++ components/dashboard/download-dialogue/citation.tsx
@@ -1,20 +1,28 @@
 export default function Citation() {
   return (
-    <div className="text-sm space-y-1">
+    <div className="text-sm space-y-1 prose prose-sm">
       <p className="underline">
         <strong>Suggested citation</strong>
       </p>
       <p>
-        Silva, J.V., Aramburu-Merlos, F., Baudron, F., Gameda, S., Meliyo, J.,
-        Ruganzu, V., Desalegn, T., Sida, T.S., Jaleta, M., Chamberlin, J.,
-        Hijmans, R.J. 2023.{" "}
+        Silva, J.V., Aramburu-Merlos, F., Baudron, F., Gameda, S., Sida, T.S.,
+        Ruganzu, V., Meliyo, J., Jaleta, M., Chamberlin, J., Hijmans, R.J. 2025.
       </p>
       <p>
         <strong>
-          The burden of acid soils for crop production in sub-Saharan Africa.
+          Soil acidity remediation in sub-Saharan Africa requires targeted
+          investments.
         </strong>
       </p>
-      <p>In preparation.</p>
+      <p>
+        <a
+          href="https://www.nature.com/articles/s43016-025-01194-z"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nature Food, 6, 799-808.
+        </a>
+      </p>
     </div>
   );
 }
